8e1eb626f457f6ef0e9af5677c6f1e7d31b74cd4
impinkflwr <LoarieJay@yandex.ru>
2021-12-23 02:36:04 +0300

    LabRab5_1 Start

---
+++ LabRab5_1/Matrix.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#pragma once
+#ifndef Matrix_Z352
+#define Matrix_Z352
+
+class Matrix
+{
+private:
+	int row, col;
+
+public:
+	double* elem;
+
+	Matrix();
+	Matrix(int _row, int _col);
+	Matrix(const Matrix& temp);
+	~Matrix();
+
+	void input();
+	void print();
+	int get_columns();
+	int get_rows();
+	int get_size();
+	double trace(double* elem, int size);
+};
+
+#endif // !Matrix_Z352
+
+
+
