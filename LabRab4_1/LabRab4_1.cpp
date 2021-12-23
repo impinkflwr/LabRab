8cfaf30f40b9b08ca2e745ab7ae7269d24fbedb3
impinkflwr <LoarieJay@yandex.ru>
2021-12-23 16:42:56 +0300

    LabRab4_1 Start

--- LabRab4_1/LabRab4_1.cpp
+++ LabRab4_1/LabRab4_1.cpp
@@ -25,6 +25,95 @@ bool Palindrom(char* str)
 	return true;
 }
 
+int find_substring1(const char* str_for_search_in, const char* substring, int start_position)
+{
+	bool tumbler;
+	for (int i = start_position; i < strlen(str_for_search_in) - strlen(substring) + 1; i++)
+	{
+		tumbler = false;
+		for (int j = 0; j < strlen(substring); j++)
+		{
+			if (str_for_search_in[i + j] == substring[j])
+			{
+				tumbler = true;
+			}
+			else
+			{
+				tumbler = false;
+				break;
+			}
+		}
+		if (tumbler == true)
+		{
+			return i;
+		}
+	}
+	return 0;
+}
+
+void find_substring2(const char* str_for_search_in, const char* substring)
+{
+	int start_position = 0, temp, n = 0;
+	while (true)
+	{
+		temp = find_substring1(str_for_search_in, substring, start_position);
+		if (temp == 0)
+		{
+			break;
+		}
+		else
+		{
+			cout << "[ " << ++n << " ] = " << temp << endl;
+			start_position = temp + 1;
+		}
+	}
+	return;
+}
+
+void encrypt(char* str_for_encrypt, int key)
+{
+	for (int i = 0; i < strlen(str_for_encrypt); i++) {
+		if ('a' <= str_for_encrypt[i] && str_for_encrypt[i] <= 'z') { // Работа с нижним регистром
+			if (str_for_encrypt[i] + key > 'z') // Если при выплнении шифра Цезаря происходит выход за конец алфавита
+				str_for_encrypt[i] = 'z' - str_for_encrypt[i] + 'a' + key - 1; // То переносим в начало и применяем ключ
+			else str_for_encrypt[i] += key; // Иначе, просто применяем ключ
+		}
+		else if ('A' <= str_for_encrypt[i] && str_for_encrypt[i] <= 'Z') { // Работа с верхним регистром
+			if (str_for_encrypt[i] + key > 'Z') // Аналогично нижнему
+				str_for_encrypt[i] = 'Z' - str_for_encrypt[i] + 'A' + key - 1;
+			else str_for_encrypt[i] += key;
+		}
+		else if (str_for_encrypt[i] == ' '); // Пропуск пробела
+	}
+	return; // Ничего не возвращает. Вывод результата шифра производится путем обращения к text
+}
+
+void marks(char* string)
+{
+	bool tumbler = false;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		if (string[i] == '"')
+		{
+			if (tumbler = false)
+			{
+				cout << "| ";
+			}
+			else
+			{
+				cout << " |";
+				tumbler = !tumbler;
+				i++;
+			}
+		}
+		if (tumbler == true)
+		{
+			cout << string[i];
+		}
+	}
+	return;
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -54,10 +143,56 @@ int main()
 			else
 			{
 				cout << Palindrom(str) << endl;
+				cout << endl;
 			}
 			break;
 
 		}
+
+		case 2:
+		{
+			char text[255];
+			char text_s[255];
+
+			cout << "Input string: "; cin.ignore(); cin.getline(text, 255);
+			char* str_for_search_in = text;
+			cout << "Input substring: "; cin.ignore(); cin.getline(text_s, 255);
+			char* substring = text_s;
+			find_substring2(str_for_search_in, substring);
+			delete[] text;
+			delete[] text_s;
+			break;
+		}
+
+		case 3:
+		{
+			int key;
+			char string[255];
+
+			cout << "Input string: "; cin.ignore(); cin.getline(string, 255);
+			cout << "Input a key: "; cin >> key;
+
+			encrypt(string, key);
+			char* str_for_encrypt = new char[strlen(string) + 1];
+			str_for_encrypt = string + '\0';
+			cout << "Encrypted string: " << str_for_encrypt << endl;
+			cout << endl;
+
+			break;
+		}
+
+		case 4:
+		{
+			char text[255];
+
+			cout << "Input string: "; cin.ignore(); cin.getline(text, 255);
+			char* string = text;
+			marks(string);
+			
+			delete[] text;
+			break;
+		}
+
 		default:
 			return 0;
 		}
