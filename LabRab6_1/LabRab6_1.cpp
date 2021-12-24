78d8f730a574f3963405f42f0104f8605ab1b8a8
impinkflwr <LoarieJay@yandex.ru>
2021-12-24 03:35:07 +0300

    LabRab6_1 Start

--- LabRab6_1/LabRab6_1.cpp
+++ LabRab6_1/LabRab6_1.cpp
@@ -1,1 +1,73 @@
-﻿
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	stack <char> steck;
+	string text;
+	cout << "Input string: ";
+	cin >> text;
+
+	for (int i = 0; i < text.size(); i++)
+	{
+		if (text[i] == '(' || text[i] == '[' || text[i] == '{')
+		{
+			steck.push(text[i]);
+		}
+		else if (text[i] == ')' || text[i] == ']' || text[i] == '}')
+		{
+			if (steck.empty())
+			{
+				cout << "It's wrong." << endl;
+				return 1;
+			}
+
+
+			switch (text[i])
+			{
+			case ')':
+				if (steck.top() == '(')
+				{
+					steck.pop();
+				}
+				else
+				{
+					cout << "It's wrong." << endl;
+					return 1;
+				}
+				break;
+
+
+			case ']':
+				if (steck.top() == '[')
+				{
+					steck.pop();
+				}
+				else
+				{
+					cout << "It's wrong." << endl;
+					return 1;
+				}
+				break;
+
+
+			case '}':
+				if (steck.top() == '{')
+				{
+					steck.pop();
+				}
+				else
+				{
+					cout << "It's wrong." << endl;
+					return 1;
+				}
+				break;
+			}
+		}
+	}
+	cout << "It's right.";
+	return 0;
+}
