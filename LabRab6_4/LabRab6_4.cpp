968d59db44088445d39adc1800b40f6fe1f175e6
impinkflwr <LoarieJay@yandex.ru>
2022-01-17 20:14:31 +0300

    LabRab6_4 Start

---
+++ LabRab6_4/LabRab6_4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstring>
+#include <set>
+
+using namespace std;
+
+int main() 
+{
+	set <string> test;		//Создаём множество
+	string input;
+	while (true)
+	{
+		cout << "Input your answers: "; cin >> input;	//Запрашиваем у пользователя ввод
+		if (test.find(input) == test.end())				//Проверяем, есть ли уже такой элемент во множестве
+		{
+			cout << "Your answers have been sent successfully!" << endl; //Если нет, то выводим успешную отпрвку
+			cout << endl;												 //И вносим элемент во множество
+			test.insert(input);
+		}																
+		else                                                             //Иначе выводим ошибку
+		{
+			cout << "Something went wrong..." << endl; "\n";
+			cout << endl;
+		}
+	}
+}
