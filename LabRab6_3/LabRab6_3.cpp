6ed43765258bf31bed0e976474bae18e891a8760
impinkflwr <LoarieJay@yandex.ru>
2022-01-17 19:26:42 +0300

    LabRab6_3 Start

---
+++ LabRab6_3/LabRab6_3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int counting(int a) {
+    while (a % 2 == 0) {
+        a = a / 2;
+    }
+    while (a % 3 == 0) {
+        a = a / 3;
+    }
+    while (a % 5 == 0) {
+        a = a / 5;
+    }
+    return a;
+}
+
+int main() {
+
+    queue <int> q;
+    int n;
+    int num = 2;
+    int x = 0;
+
+    cout << "Input number of numbers: "; cin >> n;
+    while (x < n) {
+        if (counting(num) == 1) {
+            q.push(num);
+            x++;
+        }
+        num++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << q.front() << ' ';
+        q.pop();
+    }
+
+}
